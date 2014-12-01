7c97feb66b3ed892cd3ef6b6bef20bb7482ccd2a
russellthrailkill <thrailkill39@Gmail>
2014-12-01 13:57:16 -0800

    jhmg

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -1,1 +1,29 @@
 // TODO
+game.PlayerEntity = me.Entity.extend({
+   init: function(x, y, settings) {
+       this._super(me.Entity, 'init', [x, y,  {
+           image: "mario",
+           spritttewidth: "128",
+           spriteHeight: "128",
+           width: 128,
+           height: 128,
+           getShape: function(){
+               return (new me.Rect(0, 0, 128)).toPolygon();
+           }
+       }]);
+       
+       this.body.setVelocity(5, 0);
+   },
+   
+   update: function(delta){
+      if(me.input.isKeyPressed("right") ){
+          this.body.vel.x += this.body.accel.x * me.timer.trick;
+      }else{
+          this.body.vel.x = 0;
+      }
+      
+      this.body.update(delta);
+      return true;
+   }
+    
+});
--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -5,8 +5,17 @@ game.PlayScreen = me.ScreenObject.extend({
 	onResetEvent: function() {
 		// reset the score
 		game.data.score = 0;
+                
+                me.levelDirector.loadLevel("Russell01");
+                
+                var player = me.pool.pull("mario", 0, 420, {});
+                me.game.world.addChild(player, 3)
 
-		// add our HUD to the game world
+	        me.input.bindKey(me.input.KEY.RIGHT, "right");
+            
+            
+            
+            // add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
 	},
